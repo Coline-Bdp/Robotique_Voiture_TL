049d7cb045730773f956593b572f5514e03c17e2
Coline-Bdp <baudriercoline@outlook.fr>
2023-03-16 23:11:44 +0100

    Modifications de fin de soirée

--- control_car.c
+++ control_car.c
@@ -1,13 +1,27 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int speed_m1 = 0.0;
+int speed_m2 = 0.0;
+int speed_m3 = 0.0;
+int speed_m4 = 0.0;
+
 
 int main() {
     Py_Initialize();
 
-    
     PyObject *obj = Py_BuildValue("s", "control_car.py");   //crée un objet "obj" qui est une chaîne de caractères (qui est dans le fichier Python utilisé)
     FILE *file = _Py_fopen_obj(obj, "r+");  //ouvre le fichier Python "obj" en lecture écriture avec un nom "file"
+
+    
     if(file != NULL) {      //vérifie si le pointeur de fichier est ouvert ou non
+
+    /*C'est ici qu'on va foutre le switch suivant les cas*/
+
+    //on exécutera le fichier qu'à la fin
     PyRun_SimpleFile(file, "control_car.py"); //si bien ouvert, alors on exécute le script Python
     printf("Le fichier a bien été ouvert \n");
     }
@@ -15,3 +29,25 @@ int main() {
     return 0;
 }
 
+/*Mise en place des fonctions de la voiture*/
+// 1 _ On repère le début de la ligne qu'on doit changer
+// 2 _ On supprime la ligne
+// 3 _ On remplace par la nouvelle chaîne
+
+
+
+int moving() {
+
+}
+
+int backing_up() {
+
+}
+
+int turn_right() {
+
+}
+
+int turn_left() {
+
+}
--- control_car.py
+++ control_car.py
@@ -1,8 +1,7 @@
 from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
-def controlcar():
-    kit.motor1.throttle = 1.0
-    kit.motor2.throttle = 1.0
-    kit.motor3.throttle = 1.0
-    kit.motor4.throttle = 1.0
+kit.motor1.throttle = 1.0
+kit.motor2.throttle = 1.0
+kit.motor3.throttle = 1.0
+kit.motor4.throttle = 1.0
