972df57b5c38206c7602c3ffc3e2bfb299a106b2
Coline-Bdp <baudriercoline@outlook.fr>
2023-03-16 22:06:09 +0100

    Rajout de commentaires pour des pistes

--- control_car.c
+++ control_car.c
@@ -5,38 +5,13 @@ int main() {
     Py_Initialize();
 
     
-     PyObject *obj = Py_BuildValue("s", "control_car.py");
-    FILE *file = _Py_fopen_obj(obj, "r+");
-    if(file != NULL) {
-     PyRun_SimpleFile(file, "control_car.py");
+    PyObject *obj = Py_BuildValue("s", "control_car.py");   //crée un objet "obj" qui est une chaîne de caractères (qui est dans le fichier Python utilisé)
+    FILE *file = _Py_fopen_obj(obj, "r+");  //ouvre le fichier Python "obj" en lecture écriture avec un nom "file"
+    if(file != NULL) {      //vérifie si le pointeur de fichier est ouvert ou non
+    PyRun_SimpleFile(file, "control_car.py"); //si bien ouvert, alors on exécute le script Python
+    printf("Le fichier a bien été ouvert \n");
     }
-    
-    /*
-    // Importe le module Python contenant la fonction set_motor_speeds
-    PyObject* module = PyImport_ImportModule("controlcar.py");
-    if (!module) {
-        PyErr_Print();
-        return 1;
-    }
-
-    // Obtient la fonction set_motor_speeds depuis le module
-    PyObject* set_motor_speeds_func = PyObject_GetAttrString(module, "set_motor_speeds");
-    if (!set_motor_speeds_func || !PyCallable_Check(set_motor_speeds_func)) {
-        if (PyErr_Occurred()) PyErr_Print();
-        return 1;
-    }
-
-    // Appelle la fonction set_motor_speeds avec les vitesses désirées
-    PyObject* args = Py_BuildValue("(dddd)", 0.5, 0.7, -0.2, -0.4);
-    PyObject* result = PyObject_CallObject(set_motor_speeds_func, args);
-    Py_DECREF(args);
-    Py_DECREF(result);
-
-    Py_DECREF(set_motor_speeds_func);
-    Py_DECREF(module);
-
-    Py_Finalize();
-    */
 
     return 0;
 }
+
